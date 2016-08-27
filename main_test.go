8d8b0137e2f942ace8b5bb28cbf284f937ba2b20
Itacir Ferreira Pompeu <itacir@hotmail.com>
2016-08-27 13:02:19 -0300

    add assert lib testify

--- main_test.go
+++ main_test.go
@@ -1,69 +1,44 @@
 package main
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestPersonDefined(t *testing.T) {
 	person := &Person{}
-	if person == nil {
-		t.Error("person dont defined")
-	}
+	assert.NotNil(t, person, "person dont defined")
 }
 
 func TestGetUrlDontAcceptInvaidUrl(t *testing.T) {
 	_, err := GetUrl("")
-	if err == nil {
-		t.Error("url pattern is wrong")
-	}
+	assert.Error(t, err, "url pattern is wrong")
+	assert.Equal(t, err.Error(), "invalid url", "url pattern is wrong")
 }
 
 func TestGetUrlFromServer(t *testing.T) {
 	person, err := GetUrl("http://localhost:3000")
-	if err != nil {
-		t.Error("url pattern is wrong")
-	}
 
-	if person.Email != "pompeulimp@gmail.com" {
-		t.Error("email invalido")
-	}
-
-	if person.Name != "Pompeu" {
-		t.Error("nome invalido")
-	}
-
-	if person.Idade != "33" {
-		t.Error("idade invalide")
-	}
-
-	if person.Sexo != "M" {
-		t.Error("sexo invalido")
-	}
+	assert.Nil(t, err, "url pattern is wrong")
+	assert.Equal(t, person.Email, "pompeulimp@gmail.com", "email invalido")
+	assert.Equal(t, person.Name, "Pompeu", "nome invalido")
+	assert.Equal(t, person.Idade, "33", "idade invalide")
+	assert.Equal(t, person.Sexo, "M", "sexo invalido")
 }
 
 func TestIsJson(t *testing.T) {
 	isJson := IsJson(`{"name":"pompeu"}`)
-	if !isJson {
-		t.Error("não é um json invalido")
-	}
+	assert.True(t, isJson, "não é um json invalido")
 }
 
 func TestInvalidJson(t *testing.T) {
 	isInvalid := IsJson(`{"email": "password", "jose"}`)
-	if isInvalid {
-		t.Error("esse json é valido")
-	}
+	assert.False(t, isInvalid, "esse json é valido")
 }
 
 func TestGetUrlToCsv(t *testing.T) {
 	person, err := GetUrl("http://localhost:3000/csv")
-
-	if err != nil {
-		t.Error("não é possivel pega o csv")
-	}
-
-	if person == nil {
-		t.Error("person é invalid")
-	}
+	assert.Nil(t, err, "não é possivel pega o csv")
+	assert.NotNil(t, person, "person é invalid")
 
 }
