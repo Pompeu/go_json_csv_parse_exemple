0ce398af11d4ab84fef1ee37d0d4ec2711bb7878
pompeu <itacir@hotmail.com>
2016-08-28 17:28:33 -0300

    testing for dinamic CSV

--- main_test.go
+++ main_test.go
@@ -55,3 +55,15 @@ func TestGetUrlToCsv(t *testing.T) {
 	assert.Equal(t, person.Idade, "33", "idade invalide")
 	assert.Equal(t, person.Sexo, "M", "sexo invalido")
 }
+
+func TestGetUrlCsvDynamic(t *testing.T) {
+	person, err := GetUrl("http://localhost:3000/csv/mult")
+	assert.Nil(t, err, "url pattern is wrong")
+	assert.Equal(t, person.Email, "pompeulimp@gmail.com", "email invalido")
+	assert.Equal(t, person.Name, "Pompeu", "nome invalido")
+	assert.Equal(t, person.Idade, "33", "idade invalide")
+	assert.Equal(t, person.Sexo, "M", "sexo invalido")
+	t.SkipNow()
+	assert.Equal(t, person.Outros["childrens"], "1")
+	assert.Equal(t, person.Outros["mother_name"], "Joana")
+}
